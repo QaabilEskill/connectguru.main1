054f4d786b16943969415a187767f9dd97b62d87
QaabilEskill <QaabilEskill786@gmail.com>
2026-07-02 10:41:59 +0530

    delete

--- supabase/migrations/20260526121905_6b00d145-9f7a-46a7-9929-9fd50f751361.sql
+++
@@ -1,6 +0,0 @@
-
-REVOKE EXECUTE ON FUNCTION public.handle_new_user() FROM anon, authenticated, public;
-REVOKE EXECUTE ON FUNCTION public.handle_user_metadata_update() FROM anon, authenticated, public;
-REVOKE EXECUTE ON FUNCTION public.update_payments_updated_at() FROM anon, authenticated, public;
-REVOKE EXECUTE ON FUNCTION public.update_updated_at_column() FROM anon, authenticated, public;
-REVOKE EXECUTE ON FUNCTION public.handle_psychometric_orders_updated_at() FROM anon, authenticated, public;
--- supabase/migrations/20260701114855_b6fc98c6-0ccc-4bbb-a058-5d64ebc1ee7d.sql
+++
@@ -1,2 +0,0 @@
-REVOKE EXECUTE ON FUNCTION public.get_leaderboard(int) FROM anon, public;
-GRANT EXECUTE ON FUNCTION public.get_leaderboard(int) TO authenticated;
